41f73baacd2a7f81944bf40a8b1b38758670199e
Mokua8 <amosimokua@gmail.com>
2023-02-17 13:11:57 -0800

    task10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entyr point
+ * 
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int a, i;
+
+	for (a = '0'; a <= '9'; a++)
+	{
+		for (i = '0'; i <= '9'; i++)
+		{
+			if (a < i)
+			{
+				putchar(a);
+				putchar(i);
+
+				if (a != '8' || (a == '8' && i != '9'))
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
