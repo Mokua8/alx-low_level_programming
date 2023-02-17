3ead4953df10ec28969711f1dd1add43b8d1d01c
Mokua8 <amosimokua@gmail.com>
2023-02-17 12:46:26 -0800

    correction task 2`

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>		
+#include <time.h>
 /**
  * main - Entry point
  *
@@ -9,6 +9,7 @@
 int main(void)
 {
 	char low;
+
 	for (low = 'a'; low <= 'z'; low++)
 		putchar(low);
 	putchar('\n');
