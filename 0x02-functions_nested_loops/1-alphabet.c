a302a7c2ad20e8ce81da00ee2d20ecd6f1065921
Mokua8 <amosimokua@gmail.com>
2023-02-21 11:50:44 -0800

    2nd correction on task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - check the code
+ * code that prints alphabets
+ * Return: Always 0.
+ */
+int main(void)
+{
+print_alphabet();
+putchar('\n');
+retun (0);
+}
