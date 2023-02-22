0a1251d1af913ade8ba253658a3f17adf1eeec45
Mokua8 <amosimokua@gmail.com>
2023-02-22 10:48:24 -0800

    task8 correction

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -13,7 +13,7 @@ for (hours = 0; hours < 24; hours++)
 	for (minutes = 0; minutes < 60; minutes++)
 	{
 	_putchar(hours / 10 + '0');
-	_putchar(hours % 10 = '0');
+	_putchar(hours % 10 + '0');
 	_putchar(':');
 	_putchar(minutes / 10 + '0');
 	_putchar(minutes % 10 + '0');
