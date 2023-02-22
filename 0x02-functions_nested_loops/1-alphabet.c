cc5cb9defb7fb07e95f64a5ef0ca4a194f6c7d65
Mokua8 <amosimokua@gmail.com>
2023-02-22 02:47:56 -0800

    Correcting description

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,7 +2,8 @@
 #include "main.h"
 /**
  * main - check the code
- * code that prints alphabets
+ * Description: code that prints alphabets
+ * in lowercase, followed by a niw line
  * Return: Always 0.
  */
 void print_alphabet(void)
