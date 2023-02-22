bc0d5f7ce6688bdf62b22d9a6c9474c728419651
Mokua8 <amosimokua@gmail.com>
2023-02-22 05:00:28 -0800

    task5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_sign - Entry point
+ * @n: carrier variable
+ * Description: prints sign of number
+ * Return: 1 if n>0, 0 if n==0, -1 if n<0
+ */
+int print_sign(int c)
+{
+int i = '+'
+int j = '0'
+int k = '-'
+if (n > 0)
+{
+putchar(j);
+return (1);
+}
+else if (n < 0)
+{
+putchar(k);
+return (-1);
+}
+putchar (j);
+return (0);
+}
