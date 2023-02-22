85f58a6919786e3578b43e46ffac6b2fe7b4c087
Mokua8 <amosimokua@gmail.com>
2023-02-22 09:16:51 -0800

    task5 correction

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -10,14 +10,18 @@ int print_sign(int n)
 {
 if (n > 0)
 {
-putchar('+');
+_putchar('+');
 return (1);
 }
 else if (n < 0)
 {
-putchar('-');
+_putchar('-');
 return (-1);
 }
-putchar ('0');
+else
+{
+_putchar ('0');
+return (0);
+}
 return (0);
 }
