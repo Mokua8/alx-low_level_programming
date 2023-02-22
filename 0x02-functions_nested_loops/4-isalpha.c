205bc50a182d9c77163b233ed8fd44701014b87c
Mokua8 <amosimokua@gmail.com>
2023-02-22 03:54:40 -0800

    task4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _isalpha - Entry point
+ * Description: checks for alphabetic character
+ * for the interger value it receives
+ * Return: 1 if true. 0 if false.
+ */
+int _isalpha(int c)
+{
+int i;
+for (i = 'a'||'A'; i <= 'z'||'Z'; i++)
+{
+if (c == i)
+{
+return (1);
+}
+}
+return (0);
+}
