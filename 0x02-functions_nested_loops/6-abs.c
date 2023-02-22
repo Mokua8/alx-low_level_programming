29a78ec218972ad9ed9a8084aff04ea8fb42f11f
Mokua8 <amosimokua@gmail.com>
2023-02-22 09:43:02 -0800

    task6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _abs - Entry point
+ * Description: A function that computes the absolute value
+ * @n interger
+ * Return: absolute value
+ */
+int _abs(int n)
+{
+if (n > 0)
+{
+return (n);
+}
+else if (n < 0)
+{
+return (-n);
+}
+return (0);
+}
