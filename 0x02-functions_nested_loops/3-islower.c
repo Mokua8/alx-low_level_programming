85ef0d86c009ae551ffeda8a0ee28fb11b99d68f
Mokua8 <amosimokua@gmail.com>
2023-02-22 03:36:04 -0800

    task3 correction

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -5,7 +5,7 @@
  * of the integer it receives
  * Return: 1 if true. 0 if false
  */
-int _islower(int c);
+int _islower(int c)
 {
 int i;
 for (i = 'a'; i <= 'z'; i++)
@@ -14,6 +14,7 @@ if (c == i)
 {
 return 1;
 }
+}
 return (0);
 }
-}
+
