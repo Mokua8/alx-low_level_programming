08d19b46d2038b55b1046f66d1518ef42f2619b9
Mokua8 <amosimokua@gmail.com>
2023-02-23 14:17:42 -0800

    task3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_number - Entry point
+ * Description: prints numbers from 0 - 9 followed by a new line
+ * Return: void
+ */
+
+void print_numbers(void)
+{
+	for (i = '0'; i <= '9'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+	return;
+}
