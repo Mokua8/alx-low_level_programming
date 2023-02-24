483f0689eb63a3948318e9152f9811eed4717339
Mokua8 <amosimokua@gmail.com>
2023-02-23 16:05:34 -0800

    task6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_line - Entry point
+ * Description: prints a line
+ * Return: void
+ */
+
+void print_line(int n)
+{
+	int i = 0;
+
+	while (i < n && n > 0)
+	{
+		_putchar('_');
+		i++;
+	}
+	-putchar('\n');
+}
