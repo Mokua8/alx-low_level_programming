cc543ede3903fb48071e33e729c826a1c4a4949a
Mokua8 <amosimokua@gmail.com>
2023-02-23 19:31:31 -0800

    task9correction

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -7,7 +7,7 @@
  * But for multiples of three print Fizz instead of the number and for the multiples of five print Buzz.
  * For numbers which are multiples of both three and five print FizzBuzz.
  *
- * Return: 0
+ * Return: Always 0 (Success)
  */
 
 int main(void)
@@ -18,15 +18,15 @@ int main(void)
 	{
 		if (i % 3 == 0 && i % 5 == 0)
 		{
-			printf("FizzBuzz");
+			printf("FizzBuzz ");
 		} 
 		else if (i % 3 == 0)
 		{
-			printf("Fizz");
+			printf("Fizz ");
 		}
 		else if (i % 5 == 0)
 		{
-			printf("Buzz");
+			printf("Buzz ");
 		}
 		else
 		{
