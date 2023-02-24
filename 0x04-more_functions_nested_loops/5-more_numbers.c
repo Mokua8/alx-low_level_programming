21d2477645a48539676a5a986c5f2bde0e43b37a
Mokua8 <amosimokua@gmail.com>
2023-02-23 18:16:45 -0800

    task5correction

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include "main.h"
+
 /**
  * more_numbers - Entry point
  * Description: prints 10 times the numbers, from 0 - 14, followed by a new line
  * Return: void
  */
+
 void more_numbers(void)
 {
 	char i, n;
@@ -22,8 +24,8 @@ void more_numbers(void)
 			}
 			_putchar('0' + n);
 		}
+
 		_putchar('\n');
 		j++;
 	}
-	return;
 }
