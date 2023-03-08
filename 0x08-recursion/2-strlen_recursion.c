67d97d41b416a19161812f32247c70780169e603
Mokua8 <amosimokua@gmail.com>
2023-03-08 13:05:03 -0800

    task2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - returns length of string
+ * @s: string to be measured
+ *
+ * Return: integer(lenth of string)
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	else
+	{
+		return (1 + _strlen_recursion(s + 1));
+	}
+}
