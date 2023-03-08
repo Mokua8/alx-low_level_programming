7bf0b01a0ab9947ed057fcc59284b09773af9d73
Mokua8 <amosimokua@gmail.com>
2023-03-08 09:46:19 -0800

    task0``

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -2,18 +2,18 @@
 
 /**
  * _puts_recursion - prints a string followed by a new line
- * s: string to be printed
+ * @s: string to be printed
  *
  * Return: void
  */
 
 void _puts_recursion(char *s)
 {
-	if (*s == '\0')
+	if (*s)
 	{
-		_putchar('\n');
+		_putchar(*s);
+		_puts_recursion(s + 1);
 	}
-
-	_putchar(*s);
-	_puts_recursion(s + 1);
+	else
+		_putchar('\n');
 }
