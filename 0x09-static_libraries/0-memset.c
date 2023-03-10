843b9c7f0b0961154e0da98d4a6dd4114ca640f9
Mokua8 <amosimokua@gmail.com>
2023-03-10 10:48:45 -0800

    task0

---
+++ 0x09-static_libraries/0-memset.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * *_memset - does nothing
+ * @s: _
+ * @b: _
+ * @n: _
+ * Return: null
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	return ('\0');
+}
---
+++ 0x09-static_libraries/3-islower.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _islower - Entry point
+ * Description: checks if a character is lowercase
+ * @c: the integer it receives
+ * Return: 1 if true. 0 if false
+ */
+int _islower(int c)
+{
+int i;
+for (i = 'a'; i <= 'z'; i++)
+{
+if (c == i)
+{
+return (1);
+}
+}
+return (0);
+}
+
